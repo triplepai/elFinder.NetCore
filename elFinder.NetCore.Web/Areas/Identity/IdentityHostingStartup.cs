8e441247fa354b50b0b4fe0b9a9b43de978a3f6c
pawit <pawit.sangrassamee@un.org>
2020-08-11 15:08:24 +0700

    Add publish profile Prepare identity

---
+++ elFinder.NetCore.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using elFinder.NetCore.Web.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(elFinder.NetCore.Web.Areas.Identity.IdentityHostingStartup))]
+namespace elFinder.NetCore.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                //services.AddDbContext<FileMangerDbContext>(options =>
+                //    options.UseSqlServer(
+                //        context.Configuration.GetConnectionString("FileMangerDbContextConnection")));
+
+                //services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                //    .AddEntityFrameworkStores<FileMangerDbContext>();
+            });
+        }
+    }
+}
--- elFinder.NetCore.Web/Startup.cs
+++ elFinder.NetCore.Web/Startup.cs
@@ -1,6 +1,9 @@
 ﻿using System.IO;
+using elFinder.NetCore.Web.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -32,7 +35,14 @@ namespace elFinder.NetCore.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<FileMangerDbContext>(options =>
+            options.UseSqlServer(
+                Configuration.GetConnectionString("FileMangerDbContextConnection")));
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<FileMangerDbContext>();
+
             services.AddControllersWithViews();
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,6 +61,10 @@ namespace elFinder.NetCore.Web
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseRouting();
+
+
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
